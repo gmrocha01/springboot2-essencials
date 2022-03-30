ef26a95fb888ca4ff9c253ad81695be289807327
Guilherme Machado Rocha <guilhermerocha001@outlook.com>
2022-03-30 13:43:53 -0300

    Commit dia 30/03

---
+++ src/main/java/academy/devdojo/springboot2/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,19 @@
+package academy.devdojo.springboot2.exception;
+
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails{
+    private String title;
+    private int status;
+    private String details;
+    private String developerMessage;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,58 @@
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
+import academy.devdojo.springboot2.mapper.AnimeMapper;
+import academy.devdojo.springboot2.repository.AnimeRepository;
+import academy.devdojo.springboot2.requests.AnimePostRequestBody;
+import academy.devdojo.springboot2.requests.AnimePutRequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+@RequiredArgsConstructor
+public class AnimeService {
+    private final AnimeRepository animeRepository;
+
+    public Page<Anime> listAll(Pageable pageable){
+        return animeRepository.findAll(pageable);
+
+    }
+    public List<Anime> listAllNonPageable() {
+        return animeRepository.findAll();
+    }
+
+    public Anime findByIdOrThrowBadRequestException(long id){
+        return animeRepository.findById(id).orElseThrow(() -> new BadRequestException("Anime not found"));
+
+    }
+
+    public Anime save(AnimePostRequestBody animePostRequestBody) {
+        return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
+    }
+
+    public void delete(long id) {
+        animeRepository.delete(findByIdOrThrowBadRequestException(id));
+    }
+
+    public void replace(AnimePutRequestBody animePutRequestBody) {
+        Anime savedAnime = findByIdOrThrowBadRequestException(animePutRequestBody.getId());
+        Anime anime = AnimeMapper.INSTANCE.toAnime(animePutRequestBody);
+        anime.setId(savedAnime.getId());
+        animeRepository.save(anime);
+    }
+
+    public List<Anime> findByName(String name){
+        return animeRepository.findByName(name);
+
+    }
+}
